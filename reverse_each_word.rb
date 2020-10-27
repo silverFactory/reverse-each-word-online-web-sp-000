e1d326b0f6413ade05dedade6388624556f1cdaf
Will Studabaker <will.studabaker@gmail.com>
2020-10-27 20:19:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word (string)
   arr = string.split
-  rev_arr = arr.each { |word| word.reverse  }
-  rev_arr.join("")
+  rev_arr = []
+  arr.each { |word| rev_arr << word.reverse  }
+  rev_arr.join(" ")
 end
